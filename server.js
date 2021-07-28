59c8be1ccfd8a1452800fb7c4f34e5612c6f8ed8
wallabyway <bealester@gmail.com>
2021-07-28 15:42:19 -0700

    added error codes

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ async function getLookupTable(urn, token) {
 async function getPathFromManifest(urn, token) {
 	const resp = await fetch(`${FORGE_SVF2_URL}/${urn}`, opts(token));
 	if (resp.status != 200) 
-		throw("!! PLEASE LOG IN.  GRAB A BIM360 ACCESS TOKEN");
-    const jsn = await resp.json();
+		throw(`{ "developerMessage":"Token is not provided in the request.", "moreInfo": "https://forge.autodesk.com/en/docs/oauth/v2/developers_guide/error_handling/", "errorCode": "AUTH-010"}`);
+  const jsn = await resp.json();
 	return jsn.children[0].otg_manifest;
 }
 
@@ -61,23 +61,25 @@ async function downloadLookupTable(otgm, token) {
 // ENDPOINT -------------------------------------------
 // GET {urn}/metadata/:guid/properties
 fastify.get('/:urn/metadata/:guid/properties', async (request, reply) => {
-    if (!request.headers.authorization) return "error: expected BIM360 ACCESS-TOKEN in bearer header";
-    if (!request.params.urn) return "INPUT: missing URN";
-    if (!request.params.guid) return "INPUT: missing guid";
+    if (!request.headers.authorization) return reply.code(400).send(`{ "developerMessage":"Token is not provided in the request.", "moreInfo": "https://forge.autodesk.com/en/docs/oauth/v2/developers_guide/error_handling/", "errorCode": "AUTH-010"}`);
+    if (!request.params.urn) return reply.code(400).send(`{ "developerMessage":"URN is not provided in the request."}`);
+    if (!request.params.guid) return reply.code(400).send(`{ "developerMessage":"GUID is not provided in the request."}`);
 
     const urn = request.params.urn;
     const guid = request.params.guid;
     const token = request.headers.authorization.slice(7);
 
     const dbidIdx = await getLookupTable(urn, token);
-    const res_svf2 = await proxy_fetch_properties(dbidIdx, urn, guid, token);
+    const res_svf2 = await proxy_fetch_properties(dbidIdx, urn, guid, token, reply);
 
     setCORS(reply);
     return res_svf2;
 });
 
-async function proxy_fetch_properties(dbidIdx, urn, guid, token) {
-	const json = await ( await fetch(`https://developer.api.autodesk.com/modelderivative/v2/designdata/${urn}/metadata/${guid}/properties`, opts(token) )).json();
+async function proxy_fetch_properties(dbidIdx, urn, guid, token, reply) {
+	const resp = await fetch(`https://developer.api.autodesk.com/modelderivative/v2/designdata/${urn}/metadata/${guid}/properties`, opts(token) );
+  if (resp.status != 200) return reply.callNotFound(); 
+  const json = await resp.json();
   json.data.collection.map( i => {i.svf2 = i.objectid; i.objectid = dbidIdx[i.svf2]})
 	return json;
 }
@@ -85,24 +87,25 @@ async function proxy_fetch_properties(dbidIdx, urn, guid, token) {
 // ENDPOINT -------------------------------------------
 // GET {urn}/metadata/:guid
 fastify.get('/:urn/metadata/:guid', async (request, reply) => {
-    if (!request.headers.authorization) return "error: expected BIM360 ACCESS-TOKEN in bearer header";
-    if (!request.params.urn) return "INPUT: missing URN";
-    if (!request.params.guid) return "INPUT: missing guid";
+    if (!request.headers.authorization) return reply.code(400).send(`{ "developerMessage":"Token is not provided in the request.", "moreInfo": "https://forge.autodesk.com/en/docs/oauth/v2/developers_guide/error_handling/", "errorCode": "AUTH-010"}`);
+    if (!request.params.urn) return reply.code(400).send(`{ "developerMessage":"URN is not provided in the request."}`);
+    if (!request.params.guid) return reply.code(400).send(`{ "developerMessage":"GUID is not provided in the request."}`);
 
     const urn = request.params.urn;
     const guid = request.params.guid;
     const token = request.headers.authorization.slice(7);
 
     const dbidIdx = await getLookupTable(urn, token);
-    const res_svf2 = await proxy_fetch_guid(dbidIdx, urn, guid, token);
+    const res_svf2 = await proxy_fetch_guid(dbidIdx, urn, guid, token, reply);
 
     setCORS(reply);
     return res_svf2;
 });
 
-async function proxy_fetch_guid(dbidIdx, urn, guid, token) {
-	const json = await ( await fetch(`https://developer.api.autodesk.com/modelderivative/v2/designdata/${urn}/metadata/${guid}`, opts(token) )).json();
-  //json.data.objects[0].objects.map( i => {i.svf2 = i.objectid; i.objectid = dbidIdx[i.svf2]});
+async function proxy_fetch_guid(dbidIdx, urn, guid, token, reply) {
+  const resp = await fetch(`https://developer.api.autodesk.com/modelderivative/v2/designdata/${urn}/metadata/${guid}`, opts(token) );
+  if (resp.status != 200) return reply.callNotFound(); 
+	const json = await resp.json();
 
   // recursively travese json tree and swizzle objectid with svf2
   function swizzleNode(currentNode) {
@@ -123,13 +126,15 @@ async function proxy_fetch_guid(dbidIdx, urn, guid, token) {
 // GET {urn}/metadata
 // this is a direct passthrough, no swizzling required here.
 fastify.get('/:urn/metadata', async (request, reply) => {
-    if (!request.headers.authorization) return "error: expected BIM360 ACCESS-TOKEN in bearer header";
-    if (!request.params.urn) return "INPUT: missing URN";
+    if (!request.headers.authorization) return reply.code(400).send(`{ "developerMessage":"Token is not provided in the request.", "moreInfo": "https://forge.autodesk.com/en/docs/oauth/v2/developers_guide/error_handling/", "errorCode": "AUTH-010"}`);
+    if (!request.params.urn) return reply.code(400).send(`{ "developerMessage":"URN is not provided in the request."}`);
 
     const urn = request.params.urn;
     const token = request.headers.authorization.slice(7);
 
-    const json = await ( await fetch(`https://developer.api.autodesk.com/modelderivative/v2/designdata/${urn}/metadata`, opts(token) )).json();
+    const resp = await fetch(`https://developer.api.autodesk.com/modelderivative/v2/designdata/${urn}/metadata`, opts(token) );
+    reply.code(resp.status);
+    const json = await resp.json();
 
     setCORS(reply);
     return json;
